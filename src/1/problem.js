9bcd68f121bcd0f86694ceeabfcef108053fe6e6
HoffsMH <hecker.matthew@gmail.com>
2017-11-25 18:42:35 -0500

    problem1

--- src/1/problem.js
+++ src/1/problem.js
@@ -1,3 +1,18 @@
 import _ from 'lodash';
 
-console.log('hi');
+// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+export default function problem(args = { max: 1000, div: [3, 5]}) {
+  return execute(args);
+}
+
+function execute({ max, div }) {
+  const arr = [];
+
+  _.times(max, (i) => (_.find(div, (k) => i % k ===0)) ? arr.push(i):'')
+
+  return _.sum(arr);
+}
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-import problem1 from '1/problem';
+import problems from './problems';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var menu = function () {
+  rl.question('Problem Number: ', function (answer) {
+    if (answer === 'q') return rl.close();
+    if (problems[answer]) {
+      console.log(problems[answer]())
+    }
+    menu();
+  });
+};
+
+menu();
+
