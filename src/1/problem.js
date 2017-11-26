c32795302f3b4fde47a5e94aa6e9e0d1e4ee9a94
HoffsMH <hecker.matthew@gmail.com>
2017-11-25 19:51:01 -0500

    more cleanup

--- src/1/problem.js
+++ src/1/problem.js
@@ -4,15 +4,27 @@ import _ from 'lodash';
 // we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-export default function problem(args = { max: 1000, div: [3, 5]}) {
+export default function problem(args = { max: 1000, divArr: [3, 5]}) {
   return execute(args);
 }
 
-function execute({ max, div }) {
-  const arr = [];
-
-  _.times(max, (i) => (_.find(div, (k) => i % k ===0)) ? arr.push(i):'')
-
-  return _.sum(arr);
+function execute(args) {
+  return allNumbersDivisible(args)
 }
 
+function allNumbersDivisible({ max, divArr }) {
+  const result = [];
+  const range = _.range(1, max);
+
+  return _.reduce(range, (sum, num) =>
+    isDivisibleByAny(num, divArr) ? sum + num : sum, 0);
+}
+
+function isDivisibleByAny(num, divArr) {
+  return _.find(divArr, (i) =>
+    isDivisible({ num, div: i }));
+}
+
+function isDivisible({ num, div }) {
+  return num % div === 0;
+}
